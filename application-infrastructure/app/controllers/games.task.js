8736cd76511276ccda404c26b81feaa356bb1089
Chad Kluck Dev <kl5@chadkluck.net>
2024-06-10 22:13:57 -0500

    wip: refactor

--- application-infrastructure/app/controllers/games.task.js
+++ application-infrastructure/app/controllers/games.task.js
@@ -7,77 +7,94 @@ const GamesSvc = require("../models/games.service");
 /**
  * Calls the Games Service for a list of games
  * @param {Utils.Request} REQ Request Object
- * @returns {Utils.Response} A list of games
+ * @returns {{gamechoices: Array<string>, hiddengames: Array<string>}} A list of games
  */
 const getGames = async (REQ) => {
-	const timer = new Utils.tools.Timer("Games Controller Task", true);
-	const data = await GamesSvc.get(REQ);
-	timer.stop();
-	return data;
+	return new Promise(async (resolve, reject) => {
+		const timer = new Utils.tools.Timer("Games Controller Task", true);
+		const data = await GamesSvc.get(REQ);
+		timer.stop();
+		resolve(data);
+	});
 };
 
+/**
+ * 
+ * @param {Utils.Request} REQ 
+ * @returns {string} A random or requested game by number
+ */
 const getGame = async (REQ) => {
-	let value = "";
+	return new Promise(async (resolve, reject) => {
 
-	const timer = new Utils.tools.Timer("Game Controller Task", true);
+		let value = "";
 
-	const data = await getGames(REQ);
-
-	if( data instanceof Object && "gamechoices" in data && Array.isArray(data.gamechoices) ) {
-
-		if ("play" in REQ.getProperties()) {
-			// user requested a game by number
-			let i = parseInt(REQ.getProperties().play, 10);
-
-			if (i >= 0 && i < data.gamechoices.length) {
-				// valid selection of gamechoices
-				value = data.gamechoices[i];
-			} else if (i < 0 && "hiddengames" in data && Array.isArray(data.hiddengames) && ((i*-1)+1) < data.hiddengames.length) {
-				// valid selection of hidden games
-				i = ((i*-1)+1);
-				value = data.hiddengames[i];
-			} else {
-				value = "Invalid game selection";
-			}
-		} else {
-			// pick a game based on cosmic chance
-			value = data.gamechoices[Math.floor(Math.random() * data.gamechoices.length)];
-		}
-	}
-	
-	timer.stop();
-
-	return value;
-
-};
-
-const findGame = async (REQ) => {
-	let value = null;
-
-	const timer = new Utils.tools.Timer("Find Game Controller Task", true);
-
-
-	if (REQ.getProperties()?.game) {
+		const timer = new Utils.tools.Timer("Game Controller Task", true);
 
 		const data = await getGames(REQ);
 
-		// as long as we got what we expected, pick a game based on cosmic chance
-		if( data instanceof Object && "gamechoices" in data && Array.isArray(data.gamechoices)) {
-			value = data.gamechoices.findIndex(game => game.toUpperCase() === REQ.getProperties().game.toUpperCase());
+		if( data instanceof Object && "gamechoices" in data && Array.isArray(data.gamechoices) ) {
+
+			if ("play" in REQ.getProperties()) {
+				// user requested a game by number
+				let i = parseInt(REQ.getProperties().play, 10);
+
+				if (i >= 0 && i < data.gamechoices.length) {
+					// valid selection of gamechoices
+					value = data.gamechoices[i];
+				} else if (i < 0 && "hiddengames" in data && Array.isArray(data.hiddengames) && ((i*-1)+1) < data.hiddengames.length) {
+					// valid selection of hidden games
+					i = ((i*-1)+1);
+					value = data.hiddengames[i];
+				} else {
+					value = "Invalid game selection";
+				}
+			} else {
+				// pick a game based on cosmic chance
+				value = data.gamechoices[Math.floor(Math.random() * data.gamechoices.length)];
+			}
+		}
+		
+		timer.stop();
+
+		resolve(value);
+	});
+
+};
+
+/**
+ * 
+ * @param {Utils.Request} REQ 
+ * @returns {number} The index of the requested game or null if not found
+ */
+const findGame = async (REQ) => {
+	return new Promise(async (resolve, reject) => {
+		let value = null;
+
+		const timer = new Utils.tools.Timer("Find Game Controller Task", true);
+
+
+		if (REQ.getProperties()?.game) {
+
+			const data = await getGames(REQ);
+
+			// as long as we got what we expected, pick a game based on cosmic chance
+			if( data instanceof Object && "gamechoices" in data && Array.isArray(data.gamechoices)) {
+				value = data.gamechoices.findIndex(game => game.toUpperCase() === REQ.getProperties().game.toUpperCase());
+			}
+
+			// if value is null, search hiddengames
+			if (value === null && "hiddengames" in data && Array.isArray(data.hiddengames)) {
+				let i = data.hiddengames.findIndex(game => game.toUpperCase() === REQ.getProperties().game.toUpperCase());
+				if (i) {
+					value = ((i+1)*-1);
+				}
+			}		
 		}
 
-		// if value is null, search hiddengames
-		if (value === null && "hiddengames" in data && Array.isArray(data.hiddengames)) {
-			let i = data.hiddengames.findIndex(game => game.toUpperCase() === REQ.getProperties().game.toUpperCase());
-			if (i) {
-				value = ((i+1)*-1);
-			}
-		}		
-	}
+		timer.stop();
 
-	timer.stop();
-
-	return value;
+		resolve(value);
+	});
 
 };
 
